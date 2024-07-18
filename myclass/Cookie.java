a21fe69bdd4f8b36fb14384bba3fc7d68104267c
GiftChrisJava <chrisjava77@gmail.com>
2024-07-18 14:37:03 +0200

    cookie

---
+++ myclass/Cookie.java
@@ -0,0 +1,15 @@
+class Cookie {
+    private String color;
+
+    public Cookie(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ myclass/TestCookie.java
@@ -0,0 +1,12 @@
+public class TestCookie {
+    public static void main(String[] args) {
+        Cookie c1 = new Cookie("green");
+
+        Cookie c2 = new Cookie("blue");
+        
+        System.out.println(c1.getColor());
+        System.out.println(c2.getColor());
+
+
+    }
+}
