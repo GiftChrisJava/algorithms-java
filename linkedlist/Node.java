4866e3bc089c1fab8c5bb3f15e2206a8933bc326
GiftChrisJava <chrisjava77@gmail.com>
2024-07-31 15:42:48 +0200

    print list

--- linkedlist/Node.java
+++
@@ -1,9 +0,0 @@
-// package linkedlist;
-class Node {
-    int value;
-    Node next;
-
-    Node(int value) {
-        this.value = value;
-    }
-}
---
+++ linkedlist/TestLinked.java
@@ -0,0 +1,17 @@
+// package linkedlist;
+
+/**
+ * TestLinked
+ */
+public class TestLinked {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+    }
+}
